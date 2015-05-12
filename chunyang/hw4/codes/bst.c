d0c4aebfeef0616a3559137006d14eb1f3bbb674
Tingzhi Li <vincentltz@gmail.com>
2015-05-11 17:59:06 -0700

    update

--- chunyang/hw4/codes/bst.c
+++ chunyang/hw4/codes/bst.c
@@ -271,19 +271,14 @@ struct Node *_removeLeftMost(struct Node *cur)
 	if (cur->left != NULL)
 	{
 		cur->left = _removeLeftMost(cur->left);
-		return cur;
 	}
 	else
 	{
-		struct Node *temp = cur->right;
-		free(cur);
-		return temp;
-        /*
         struct Node *temp = cur;
         cur = cur->right;
         free(temp);
-        return cur;*/
 	}
+    return cur;
 }
 
 
@@ -489,7 +484,7 @@ void test_addNode_1() {
     printTree(tree);
     printf("\n");
     
-    deleteBSTree(tree);
+    //deleteBSTree(tree);
 
 }
 
@@ -576,8 +571,14 @@ void testAddNode_1() {
     printf("Calling printTree function...\n");
     printTree(tree);
     printf("\n");
-
-    deleteBSTree(tree);
+    /*
+    printNode(tree->root);
+    printNode(tree->root->left);
+    printNode(tree->root->right);
+    printNode(tree->root->right);
+    printNode(tree->root->right->left);
+    */
+    //deleteBSTree(tree);
 }
 
 /*
@@ -636,7 +637,7 @@ void testLeftMost_1() {
     
 	printTestResult(compare(_leftMost(tree->root->right), &myData3) == 0, "_leftMost", "left most of right of root");
     
-    deleteBSTree(tree);
+    //deleteBSTree(tree);
 }
 
 void testRemoveLeftMost_1() {
@@ -658,10 +659,10 @@ void testRemoveLeftMost_1() {
    
  	cur = _removeLeftMost(tree->root);
     printTestResult(cur == tree->root, "_removeLeftMost", "removing leftmost of root 2st try");
-    /*
+    
     printf("Calling printTree function...\n");
     printTree(tree);
-    printf("\n");*/
+    printf("\n");
     
 }
 
@@ -713,15 +714,37 @@ void testFirstDataStructure() {
     
     printf("\n");
     testRemoveNode_1();
+    
+    printf("\n");
+    //testRemoveBSTree_1();
 
 }
 
 /*
 void testSecondDataStructure() {
-    
-}
- 
-*/
+    printf("\n");
+    printf("Testing second data structure begin...\n");
+
+    test_addNode_2();
+    printf("\n");
+
+    testAddNode_2();
+
+    printf("\n");
+    testContainsBSTree_2();
+
+    printf("\n");
+    testLeftMost_2();
+
+    printf("\n");
+    testRemoveLeftMost_2();
+
+    printf("\n");
+    testRemoveNode_2();
+
+    printf("\n");
+    testRemoveBSTree_2();
+}*/
 
 /*
 
